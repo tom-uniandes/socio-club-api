5345ad8e95bca160c2422e7db9aed52d081f3060
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-09-15 14:03:56 -0500

    Init porject nest

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { VersioningType } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+ const app = await NestFactory.create(AppModule);
+ app.enableVersioning({
+   type: VersioningType.URI,
+   prefix: 'api/v',
+   defaultVersion: '1',
+ });
+ await app.listen(3000);
+}
+bootstrap();
