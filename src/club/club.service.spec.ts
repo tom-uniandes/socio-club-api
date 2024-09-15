88f19a0fe29353578db6f663438df59f086f5158
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-09-15 16:26:16 -0500

    Add test to findAll

--- src/club/club.service.spec.ts
+++ src/club/club.service.spec.ts
@@ -4,10 +4,12 @@ import { ClubService } from './club.service';
 import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { ClubEntity } from './club.entity/club.entity';
+import { faker } from '@faker-js/faker';
 
 describe('ClubService', () => {
   let service: ClubService;
   let repository: Repository<ClubEntity>;
+  let clubsList: ClubEntity[] = [];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +19,30 @@ describe('ClubService', () => {
 
     service = module.get<ClubService>(ClubService);
     repository = module.get<Repository<ClubEntity>>(getRepositoryToken(ClubEntity));
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    clubsList = [];
+    for(let i = 0; i < 5; i++){
+        const club: ClubEntity = await repository.save({
+        nombre: faker.company.name(),
+        fecha_fundacion: faker.date.anytime(),
+        imagen: faker.image.url(),
+        descripcion: faker.word.words(2)})
+        clubsList.push(club);
+    }
+  }
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('findAll should return all clubs', async () => {
+    const clubs: ClubEntity[] = await service.findAll();
+    expect(clubs).not.toBeNull();
+    expect(clubs).toHaveLength(clubsList.length);
+  });
 });
+
--- src/socio/socio.service.spec.ts
+++ src/socio/socio.service.spec.ts
@@ -4,10 +4,12 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { TypeOrmTestingConfig } from '../shared/testing-utils/typeorm-testing-config';
 import { SocioEntity } from './socio.entity/socio.entity';
+import { faker } from '@faker-js/faker';
 
 describe('SocioService', () => {
   let service: SocioService;
   let repository: Repository<SocioEntity>;
+  let sociosList: SocioEntity[] = [];
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -17,9 +19,28 @@ describe('SocioService', () => {
 
     service = module.get<SocioService>(SocioService);
     repository = module.get<Repository<SocioEntity>>(getRepositoryToken(SocioEntity));
+    await seedDatabase();
   });
+
+  const seedDatabase = async () => {
+    repository.clear();
+    sociosList = [];
+    for(let i = 0; i < 5; i++){
+        const socio: SocioEntity = await repository.save({
+        nombre_usuario: faker.person.firstName(),
+        correo_electronico: faker.internet.email(),
+        fecha_nacimiento: faker.date.anytime()})
+        sociosList.push(socio);
+    }
+  }
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findAll should return all socios', async () => {
+    const socios: SocioEntity[] = await service.findAll();
+    expect(socios).not.toBeNull();
+    expect(socios).toHaveLength(sociosList.length);
   });
 });
