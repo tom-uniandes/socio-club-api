4bc8754b92a17156c13d093e218f208a7454ded7
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-09-15 22:26:57 -0500

    Add methods in controller clubs

--- src/club/club.controller.ts
+++ src/club/club.controller.ts
@@ -1,7 +1,41 @@
-import { Controller, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put, UseInterceptors } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { BusinessErrorsInterceptor } from '../shared/interceptors/business-errors/business-errors.interceptor';
 import { ClubService } from './club.service';
+import { ClubDto } from './club.dto/club.dto';
+import { ClubEntity } from './club.entity/club.entity';
+
 
 @Controller('clubs')
 @UseInterceptors(BusinessErrorsInterceptor)
-export class ClubController {constructor(private readonly clubService: ClubService) {}}
+export class ClubController {constructor(private readonly clubService: ClubService) {}
+
+    @Get()
+    async findAll() {
+        return await this.clubService.findAll();
+    }
+
+    @Get(':clubId')
+    async findOne(@Param('clubId') clubId: string) {
+        return await this.clubService.findOne(clubId);
+    }
+
+    @Post()
+    async create(@Body() clubDto: ClubDto) {
+        const club: ClubEntity = plainToInstance(ClubEntity, clubDto);
+        return await this.clubService.create(club);
+    }
+
+    @Put(':clubId')
+        async update(@Param('clubId') clubId: string, @Body() clubDto: ClubDto) {
+        const club: ClubEntity = plainToInstance(ClubEntity, clubDto);
+        return await this.clubService.update(clubId, club);
+    }
+
+    @Delete(':clubId')
+    @HttpCode(204)
+        async delete(@Param('clubId') clubId: string) {
+        return await this.clubService.delete(clubId);
+    }
+
+}
