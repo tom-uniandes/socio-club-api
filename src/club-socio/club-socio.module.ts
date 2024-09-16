961c9578e8428ff228f7fe52efdec503b737f2bc
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-09-15 22:51:57 -0500

    Add import SocioEntity in club-socio

--- src/club-socio/club-socio.module.ts
+++ src/club-socio/club-socio.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ClubSocioService } from './club-socio.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClubEntity } from '../club/club.entity/club.entity';
+import { SocioEntity } from '../socio/socio.entity/socio.entity';
 import { ClubSocioController } from './club-socio.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ClubEntity])],
+  imports: [TypeOrmModule.forFeature([ClubEntity, SocioEntity])],
   providers: [ClubSocioService],
   controllers: [ClubSocioController]
 })
